8c2d192f4fd1f090e392dcf762e44315f40d0efe
pulak <destisdbest@gmail.com>
2024-04-03 11:03:04 +0530

    added firebase

--- services/firebase-config.jsx
+++ services/firebase-config.jsx
@@ -13,7 +13,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 export default db;
